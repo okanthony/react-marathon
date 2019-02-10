5d896b187cbf8581199a7518a074b59be00cdcf0
okanthony <degennaro.anthony@gmail.com>
2019-02-10 15:26:31 -0500

    feat: add pages dir

---
+++ src/pages/Page1/index.jsx
@@ -0,0 +1,12 @@
+// import './styles.css';
+import React from 'react'
+
+const Page1 = () => {
+    return (
+        <div className="page">
+            Page 1
+        </div>
+    );
+}
+
+export { Page1 };
---
+++ src/pages/Page2/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page2 = () => {
+    return (
+        <div className="page">
+            Page 1
+        </div>
+    );
+};
+
+export { Page2 };
