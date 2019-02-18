9de3f6c8a6e51c00a0bce42ea02ca90503b08a0d
okanthony <degennaro.anthony@gmail.com>
2019-02-18 12:06:08 -0500

    refactor: App
    add Context, Page

--- src/admin/App/index.js
+++ src/admin/App/index.js
@@ -1,17 +1,21 @@
 import './styles.css';
-import React from 'react';
+import React, { useContext } from 'react';
 
+import { AppContext } from '../Context';
 import { TitleBar } from '../TitleBar';
 import { Instructions } from '../Instructions';
+import { Page } from '../Page';
 
 const App = () => {
+    const { currentPage } = useContext(AppContext);
+
     return (
         <div className="app-container">
             <TitleBar />
             <div className="app-body">
                 <Instructions />
                 <div className="app-content">
-                    Foobas
+                    <Page currentPage={currentPage} />
                 </div>
             </div>
         </div>
