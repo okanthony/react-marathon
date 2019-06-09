49aae6803e4ccb5a179a81c04976799d6a58da48
okanthony <degennaro.anthony@gmail.com>
2019-06-09 14:52:26 -0400

    feat: add ids to Page components

--- src/pages/Page1/index.js
+++ src/pages/Page1/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Page1 = () => {
-  return <div className="page">Page 1</div>;
+  return (
+    <div className="page" id="page1">
+      Page 1
+    </div>
+  );
 };
 
 export { Page1 };
--- src/pages/Page2/index.js
+++ src/pages/Page2/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Page2 = () => {
-  return <div className="page">Page 2</div>;
+  return (
+    <div className="page" id="page2">
+      Page 2
+    </div>
+  );
 };
 
 export { Page2 };
